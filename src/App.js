5725af7a72b9dae29099beca755021b46bf17a56
LJKR2020 <loredana.rietveld@gmail.com>
2021-12-01 16:31:02 +0100

    added buttons and buttoncomponent

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import Button from "./components/button";
 import './App.css';
 
 function App() {
-  return (
-      <h1>Handbags & Purses</h1>
-  );
+    return (
+        <>
+            <h1>Handbags & Purses</h1>
+            <nav>
+                <Button
+                    buttonmessage="to the collection"
+                    buttontitle="to the collection"
+                    isdisabled={false}
+                />
+                <Button
+                    buttonmessage="shop all bags"
+                    buttontitle="shop all bags"
+                    isdisabled={false}
+                />
+                <Button
+                    buttonmessage="pre-order"
+                    buttontitle="pre-order"
+                    isdisabled={true}
+                />
+            </nav>
+        </>
+    );
 }
 
 export default App;
